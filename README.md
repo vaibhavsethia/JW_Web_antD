b8be29e6edf0113f5f08b043c91bcb028cf90cd6
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-08-24 21:23:14 +0530

    Setup

---
+++ README.md
@@ -0,0 +1,9 @@
+## What is it for ?
+
+Github repository for J williamson company website.
+
+## How to run ?
+
+- git clone the repo
+- yarn install
+- yarn start
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <Navbar />;
+  }
+}
+
+export default App;
