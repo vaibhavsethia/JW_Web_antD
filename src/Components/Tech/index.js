e36b520d708a7a796f79a0a6ab39a6491bc2535e
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-08-28 00:17:52 +0530

    tech setup

---
+++ src/Components/Tech/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Header from './Header'
+
+// const TechStack = [
+//   {
+//     Name: <span><span style={{'fontSize':'34px', 'color': '#5657DD'}}>A</span>mazon Web Services</span>,
+//     Icon: "fas fa-microchip",
+//     Desc: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat.",
+//   }, {
+//     Name: <span><span style={{'fontSize':'34px', 'color': '#5657DD'}}>R</span>eact</span>,
+//     Icon: "fas fa-laptop",
+//     Desc: "One of the main technologies we use for our frontend is React alongwith UIs. Some of our projects that use this are JP Chit-Chat and JP Translator",
+//   }, {
+//     Name: <span><span style={{'fontSize':'34px', 'color': '#5657DD'}}>P</span>ython</span>,
+//     Icon: "fas fa-code",
+//     Desc: "what makes our platform standout other similar platforms and companies is the strong Machine Learning architecture a large chunk of which will be coded in Python.",
+//   }, {
+//     Name: <span><span style={{'fontSize':'34px', 'color': '#5657DD'}}>J</span>ava</span>,
+//     Icon: "fas fa-code",
+//     Desc: "Coding creative pluggins for Minecraft Java Edition is obviously done in Java. Minecraft being one of those games with loads of oppurtunities provides our creative team with loads of ideas too.",
+//   }, {
+//     Name: <span><span style={{'fontSize':'34px', 'color': '#5657DD'}}>G</span>raphQl</span>,
+//     Icon: "fas fa-database",
+//     Desc: "GraphQL alongwith AWS Appsync helps simplifying and building a large part of our backend. It is one of the main Component in the JP Chit-Chat.",
+//   }, {
+//     Name: <span><span style={{'fontSize':'34px', 'color': '#5657DD'}}>A</span>pollo</span>,
+//     Icon: "fas fa-server",
+//     Desc: "The Connection between our frontend and backend is done by Apollo. Simplifying our developers work is all it does. Its implementation can be found in most of our projects.",
+//   },
+// ]
+
+export class Tech extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+      </div>
+    )
+  }
+}
+
+export default Tech
