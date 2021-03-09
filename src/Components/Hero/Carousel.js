435e6080593139bfa940b12d9ef39a3a51b7c24e
vaibhavsethia <vaibhavsethia1998@gmail.com>
2021-03-09 23:42:48 +0530

    carousel changes

--- src/Components/Hero/Carousel.js
+++ src/Components/Hero/Carousel.js
@@ -11,50 +11,159 @@ class Carousel extends React.Component {
   render() {
     return (
       <BannerAnim prefixCls="banner-user">
-        <Element prefixCls="banner-user-elem" key="0">
-          <BgElement key="bg" className="bg banner-4" />
-          <TweenOne
-            className="banner-user-title"
-            animation={{ y: 30, opacity: 0, type: "from" }}
-          >
-            J Williamson
+        <Element key="1"
+          prefixCls="banner-user-elem"
+        >
+          <BgElement
+            key="bg"
+            className="bg"
+            style={{
+              backgroundImage: `url(https://megaminehostedimages.s3.amazonaws.com/GameModes/Paintball/1500x2000/Cathedral/Cathedral_1.jpg)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+          J Williamson
           </TweenOne>
-          <TweenOne
-            className="banner-user-text"
-            animation={{ y: 30, opacity: 0, type: "from", delay: 100 }}
+          <TweenOne 
+            className="banner-user-text" 
+            animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
           >
             You Play, We Develop
-          </TweenOne>
+            </TweenOne>
         </Element>
-        <Element prefixCls="banner-user-elem" key="1">
-          <BgElement key="bg" className="bg banner-2" />
-          <TweenOne
-            className="banner-user-title"
-            animation={{ y: 30, opacity: 0, type: "from" }}
-          >
-            Using AI and ML
+        <Element key="2"
+          prefixCls="banner-user-elem"
+        >
+          <BgElement
+            key="bg"
+            className="bg"
+            style={{
+              backgroundImage: `url(https://megaminehostedimages.s3.amazonaws.com/GameModes/Bed_Wars/1500x2000/JPEG/Honeybloom.jpg)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+          ESports and Enhanced Gameplay
           </TweenOne>
-          <TweenOne
-            className="banner-user-text"
-            animation={{ y: 30, opacity: 0, type: "from", delay: 100 }}
+          <TweenOne 
+            className="banner-user-text" 
+            animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
           >
-            In Gaming for an immersive experience
-          </TweenOne>
+            To keep creators motivated
+            </TweenOne>
         </Element>
-        <Element prefixCls="banner-user-elem" key="2">
-          <BgElement key="bg" className="bg banner-3" />
-          <TweenOne
-            className="banner-user-title"
-            animation={{ y: 30, opacity: 0, type: "from" }}
-          >
-            Competitive Gaming
+        <Element key="3"
+          prefixCls="banner-user-elem"
+        >
+          <BgElement
+            key="bg"
+            className="bg"
+            style={{
+              backgroundImage: `url(https://megaminehostedimages.s3.amazonaws.com/GameModes/Bed_Wars/1500x2000/JPEG/Candy_Island.jpg)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+          Entertainment Platform
           </TweenOne>
-          <TweenOne
-            className="banner-user-text"
-            animation={{ y: 30, opacity: 0, type: "from", delay: 100 }}
+          <TweenOne 
+            className="banner-user-text" 
+            animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
+          >
+            For Youtube and Twitch Content Creators
+            </TweenOne>
+        </Element>
+        <Element key="4"
+          prefixCls="banner-user-elem"
+        >
+          <BgElement
+            key="bg"
+            className="bg"
+            style={{
+              backgroundImage: `url(https://megaminehostedimages.s3.amazonaws.com/GameModes/Bed_Wars/1500x2000/JPEG/Candy_Island.jpg)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+          Simple but Creative Game Modes
+          </TweenOne>
+          <TweenOne 
+            className="banner-user-text" 
+            animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
+          >
+            To keep you interested and enthusiastic
+            </TweenOne>
+        </Element>
+        <Element key="5"
+          prefixCls="banner-user-elem"
+        >
+          <BgElement
+            key="bg"
+            className="bg"
+            style={{
+              backgroundImage: `url(https://megaminehostedimages.s3.amazonaws.com/GameModes/Duels/1500x2000/JPEG/Clay_Mounds.jpg)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+          Custom 3D Models and Items
+          </TweenOne>
+          <TweenOne 
+            className="banner-user-text" 
+            animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
+          >
+            To make your gaming experience personalized
+            </TweenOne>
+        </Element>
+        <Element key="6"
+          prefixCls="banner-user-elem"
+        >
+          <BgElement
+            key="bg"
+            className="bg"
+            style={{
+              backgroundImage: `url(https://megaminehostedimages.s3.amazonaws.com/GameModes/Parkour/1500x2000/JPEG/Time.jpg)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+          Competitive Gaming
+          </TweenOne>
+          <TweenOne 
+            className="banner-user-text" 
+            animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
           >
             and many new modes in Minecraft
+            </TweenOne>
+        </Element>
+        <Element key="7"
+          prefixCls="banner-user-elem"
+        >
+          <BgElement
+            key="bg"
+            className="bg"
+            style={{
+              backgroundImage: `url(https://megaminehostedimages.s3.amazonaws.com/GameModes/King_of_the_hill/1500x2000/JPEG/Death_Valley.jpg)`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <TweenOne className="banner-user-title" animation={{ y: 30, opacity: 0, type: 'from' }}>
+          Using AI and ML
           </TweenOne>
+          <TweenOne 
+            className="banner-user-text" 
+            animation={{ y: 30, opacity: 0, type: 'from', delay: 100 }}
+          >
+            In Gaming for an immersive experience
+            </TweenOne>
         </Element>
       </BannerAnim>
     );
