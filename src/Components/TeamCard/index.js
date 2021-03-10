c02693294315125d299780db855140736e6fae53
vaibhavsethia <vaibhavsethia1998@gmail.com>
2021-03-10 20:49:19 +0530

    Team page

--- src/Components/TeamCard/index.js
+++ src/Components/TeamCard/index.js
@@ -1,38 +1,29 @@
 import React, { Component } from 'react'
-import { Divider, Col } from 'antd'
-
-import { More } from './More';
+import { Divider, Col, Row } from 'antd'
+import Flip from 'react-reveal/Flip';
+import Bounce from 'react-reveal/Bounce';
 
 import './styles.css'
 
 export class TeamCard extends Component {
   render() {
     return (
-      <Col className="tc" xl={6} lg={8} md={12} sm={12} xs={24}>
-        <div className="pt3 pl3 pr3 tc">
-          <img alt="profile" className="dimension br3 shadow-2 grow" src={this.props.Image}></img>
-          <div className="pa2 content br3">
-            <span className="tl col-gray">{this.props.Name}</span><Divider type="vertical"/><span className="tr col-teal">{this.props.Role}</span>
-            <div className="tc col-gray f4 mt1">
-              {
-                this.props.Github === ""
-                ?<div></div>
-                :<a href={this.props.Github}><i className="grow link-hover fab fa-github mr2 git-link"></i></a>
-              }
-              {
-                this.props.LinkedIn === ""
-                ?<div></div>
-                :<a href={this.props.LinkedIn}><i className="ml2 grow link-hover fab fa-linkedin"></i></a>
-              }
-            </div>
-            <div>
-              <More 
-                Desc = {this.props.Desc}
-              />
-            </div>
-          </div>
-        </div>
-      </Col>
+      <Row className="pl5 pr4 mt3 mb3">
+        <Col md={4}>
+            <img alt="profile" className="dimension br3 shadow-2 grow br-100" src={this.props.Image}></img>
+          </Col> 
+        <Col md={20} className="mt4">
+          <span className="f3 tl col-gray"><Flip right cascade>{this.props.Name}</Flip></span><Divider type="vertical"/><span className="f4 tr col-teal"><Flip left cascade>{this.props.Role}</Flip></span>
+          <br />
+          {
+            this.props.Github === ""
+            ?<div></div>
+            :<a href={this.props.Github}><i className="f4 grow link-hover fab fa-github mr2 git-link"></i></a>
+          }
+          <br/>
+          <p className=" f5 col-gray mt1 tl pr1 pl1"><Bounce right cascade>{this.props.Desc}</Bounce></p>
+        </Col>
+      </Row>
     )
   }
 }
--- src/Views/Team.js
+++ src/Views/Team.js
@@ -3,14 +3,12 @@ import { Row, Tabs } from 'antd';
 
 import SectionHeader from '../Utils/SectionHeader'
 import TeamCard from '../Components/TeamCard';
-import Arnav from '../Images/Profile/Arnav.jpg'
-import Jon from '../Images/Profile/Jon.jpeg'
-import Prashant from '../Images/Profile/Prashant.jpg'
-import Sid from '../Images/Profile/Sid.jpeg'
-import Vaibhav from '../Images/Profile/Vaibhav.jpg'
-import Vivek from '../Images/Profile/Vivek.jpg'
-import Kishan from '../Images/Profile/Kishan.jpg'
-import Kaushal from '../Images/Profile/Kaushal.png'
+import Jon from '../Images/Profile/Jon.png'
+import Prashant from '../Images/Profile/Prashant.png'
+import Sid from '../Images/Profile/Sid.png'
+import Vaibhav from '../Images/Profile/Vaibhav.png'
+import Vivek from '../Images/Profile/Vivek.png'
+import Kishan from '../Images/Profile/Kishan.png'
 import Mridul from '../Images/Profile/Mridul.jpg'
 
 const { TabPane } = Tabs;
@@ -24,98 +22,63 @@ export class Team extends Component {
           Tagline = {<span>None of us ever do great things. But we can all do small things,<br/> with great love, and together we can do something wonderful.</span>}
           AnimType="None"
         />
-        <Row className="tc">
-        <Tabs className="tab" defaultActiveKey="1" centered>
-          <TabPane tab={<span className="f4 fw3">Veteran Team</span>} key="1">
-            <Row className="tc mb2 mt1">
-              <TeamCard 
-                Image = {Jon}
-                Name = {<span>Jonathon Williamson</span>}
-                Role = {<span>CFO</span>}
-                LinkedIn = "https://www.linkedin.com/in/jonathan-p-williamson/"
-                Github = ""
-                Desc = {<span>A Passionate inventor and clever problem solver who doesn’t shy from ‘doing hard things.’ Eager learner and entrepreneurial, “edge” thinker who approach initiatives with a gritty work ethic and an intrepid spirit of curiosity.</span>}
-              />
-              <TeamCard 
-                Image = {Sid}
-                Name = {<span>Sidharth Sachdeva</span>}
-                Role = {<span>CFO</span>}
-                LinkedIn = "https://www.linkedin.com/in/siddharth-sachdeva-01/"
-                Github = ""
-                Desc = {<span>An Agile Professional and Scrum Master who is passionate about Entrepreneurship as well as having a holistic approach towards Software Development and Engineering.</span>}
-              />
-              <TeamCard 
-                Image = {Vaibhav}
-                Name = {<span>Vaibhav Sethia</span>}
-                Role = {<span>Full Stack Developer</span>}
-                LinkedIn = "https://www.linkedin.com/in/vaibhav-sethia-4711b8145/"
-                Github = "https://github.com/vaibhavsethia"
-                Desc = {<span>Some people work to live but he lives to work. A night owl, gamer and sports lover. A coder who loves to play in new technologies, best for small talks and caffeine shots! He is always up for discussions around science fiction and some useless fact.</span>}
-              />
-              <TeamCard 
-                Image = {Vivek}
-                Name = {<span>Vivek Sharma</span>}
-                Role = {<span>Dev Ops</span>}
-                LinkedIn = "https://www.linkedin.com/in/vivek-sharma-33592a98/"
-                Github = "https://github.com/Baazigar007"
-                Desc = {<span>A Dr. Strange fan, you don't go to him for a second opinion, you go to him for a better opinion. A street smart guy good at bargaining (Trust me, I've seen him bargain with Dormammu). Focussed and values time, as the protector of time stone. His sacred relic? Laptop. His preferred dimension? Dark Dimension.</span>}
-              />
-            </Row>
-            <TeamCard 
-                Image = {Prashant}
-                Name = {<span>Prashant Gaurav</span>}
-                Role = {<span>Full Stack Developer</span>}
-                LinkedIn = "https://www.linkedin.com/in/prashant-gaurav-01/"
-                Github = "https://github.com/pg711700"
-                Desc = {<span>His day starts with a big dreams to develop and ends with debugging. The the things that attract him towards developing stuff is how cool IDE’ s are these days, just type an @ and the code writes itself.He is a passionate coder, traveller, reader. He makes weird faces in front of camera and can’ t sing or dance.For him the biggest question is that# isP = NP ? </span>}
-              />
-              <TeamCard 
-                Image = {Arnav}
-                Name = {<span>Arnav Anand</span>}
-                Role = {<span>ML & AI Developer</span>}
-                LinkedIn = "https://www.linkedin.com/in/arnav-anand-a6797714b/"
-                Github =  "https://github.com/ArgonArnav"
-                Desc = {<span>An Ambitious, Adaptive, Aspirant & Agile engineer who enjoys writing code, loves to face challenges and strive to do the best with whatever task or work is presented, in order to get the best results. Zealous learner with a Keep On Improving state of mind. Currently engrossed in the field of AI and planning to build a future around it.</span>}
-              />    
-          </TabPane>
-          <TabPane tab={<span className="f4 fw3">Seasoned Team</span>} key="2">
-            <Row className="tc mb2 mt1">
-              <TeamCard 
-                Image = {Kishan}
-                Name = {<span>Kishan</span>}
-                Role = {<span>Lead Modeller</span>}
-                LinkedIn = ""
-                Github = ""
-                Desc = {<span>An enthusiastic manga reader and anime watcher. Throughout the day you'll find him either playing games or supporting fc barcelona(#Força Barça) or hanging out with his friends on discord.Hopes to to becomes 1500 rated on chess.com.Loves everything 3d and animation.</span>}
-              />
-              <TeamCard 
-                Image = {Kaushal}
-                Name = {<span>Kaushal Gawri</span>}
-                Role = {<span>Java Developer</span>}
-                LinkedIn = "https://www.linkedin.com/in/kaushal-gawri-596aba159"
-                Github = "https://github.com/kaushal-gawri9899"
-                Desc = {<span>A passionate programmer with good problem solving and analytical skills. He's an avid learner and is curious about how things are build . He likes to juggle between multiple tasks while keeping his priorities in check. He likes to 'orchestrate his mornings to the time of coffee.</span>}
-              />
-              <TeamCard 
-                Image = {Mridul}
-                Name = {<span>Mridul Gupta</span>}
-                Role = {<span>Lead Java Developer</span>}
-                LinkedIn = "https://www.linkedin.com/in/mridulcse27"
-                Github = "https://github.com/Mrd278"
-                Desc = {<span>A cricket lover, loves to play and code the games. Passionate about the work. wants to enhance the experience of gaming with advanced technologies. Wants the best out of the rest.</span>}
-              />
-              {/* <TeamCard 
-                Image = {Harshit}
-                Name = {<span>Harshit Bargujar</span>}
-                Role = {<span>Modeller</span>}
-                LinkedIn = "https://www.linkedin.com/in/harshitbargujar/"
-                Github = "https://github.com/harshit2608"
-                Desc = {<span></span>}
-              /> */}
-            </Row>
-          </TabPane>
-        </Tabs>
-        </Row>
+
+        <TeamCard 
+          Image = {Jon}
+          Name = {<span>Jonathon Williamson</span>}
+          Role = {<span>CFO</span>}
+          LinkedIn = "https://www.linkedin.com/in/jonathan-p-williamson/"
+          Github = ""
+          Desc = {<span>A Passionate inventor and clever problem solver who doesn’t shy from ‘doing hard things.’ Eager learner and entrepreneurial, “edge” thinker who approach initiatives with a gritty work ethic and an intrepid spirit of curiosity.</span>}
+        />
+        <TeamCard 
+          Image = {Sid}
+          Name = {<span>Sidharth Sachdeva</span>}
+          Role = {<span>CFO</span>}
+          LinkedIn = "https://www.linkedin.com/in/siddharth-sachdeva-01/"
+          Github = ""
+          Desc = {<span>An Agile Professional and Scrum Master who is passionate about Entrepreneurship as well as having a holistic approach towards Software Development and Engineering.</span>}
+        />
+        <TeamCard 
+          Image = {Vaibhav}
+          Name = {<span>Vaibhav Sethia</span>}
+          Role = {<span>Full Stack Developer</span>}
+          LinkedIn = "https://www.linkedin.com/in/vaibhav-sethia-4711b8145/"
+          Github = "https://github.com/vaibhavsethia"
+          Desc = {<span>Some people work to live but he lives to work. A night owl, gamer and sports lover. A coder who loves to play in new technologies, best for small talks and caffeine shots! He is always up for discussions around science fiction and some useless fact.</span>}
+        />
+        <TeamCard 
+          Image = {Vivek}
+          Name = {<span>Vivek Sharma</span>}
+          Role = {<span>Dev Ops</span>}
+          LinkedIn = "https://www.linkedin.com/in/vivek-sharma-33592a98/"
+          Github = "https://github.com/Baazigar007"
+          Desc = {<span>A Dr. Strange fan, you don't go to him for a second opinion, you go to him for a better opinion. A street smart guy good at bargaining (Trust me, I've seen him bargain with Dormammu). Focussed and values time, as the protector of time stone. His sacred relic? Laptop. His preferred dimension? Dark Dimension.</span>}
+        />
+        <TeamCard 
+          Image = {Prashant}
+          Name = {<span>Prashant Gaurav</span>}
+          Role = {<span>Full Stack Developer</span>}
+          LinkedIn = "https://www.linkedin.com/in/prashant-gaurav-01/"
+          Github = "https://github.com/pg711700"
+          Desc = {<span>His day starts with a big dreams to develop and ends with debugging. The the things that attract him towards developing stuff is how cool IDE’ s are these days, just type an @ and the code writes itself.He is a passionate coder, traveller, reader. He makes weird faces in front of camera and can’ t sing or dance.For him the biggest question is that# isP = NP ? </span>}
+        />
+        <TeamCard 
+          Image = {Kishan}
+          Name = {<span>Kishan</span>}
+          Role = {<span>Lead Modeller</span>}
+          LinkedIn = ""
+          Github = ""
+          Desc = {<span>An enthusiastic manga reader and anime watcher. Throughout the day you'll find him either playing games or supporting fc barcelona(#Força Barça) or hanging out with his friends on discord.Hopes to to becomes 1500 rated on chess.com.Loves everything 3d and animation.</span>}
+        />
+        <TeamCard 
+          Image = {Mridul}
+          Name = {<span>Mridul Gupta</span>}
+          Role = {<span>Lead Java Developer</span>}
+          LinkedIn = "https://www.linkedin.com/in/mridulcse27"
+          Github = "https://github.com/Mrd278"
+          Desc = {<span>A cricket lover, loves to play and code the games. Passionate about the work. wants to enhance the experience of gaming with advanced technologies. Wants the best out of the rest.</span>}
+        />
       </div>
     )
   }
