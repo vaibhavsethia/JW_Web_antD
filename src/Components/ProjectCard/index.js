2204c5742c60b32d2adf58363114b78003954b20
vaibhavsethia <vaibhavsethia1998@gmail.com>
2021-03-12 16:01:14 +0530

    initialized project cards

---
+++ src/Components/ProjectCard/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './styles.css'
+
+export class index extends Component {
+  render() {
+    return (
+      <div>
+        Project Cardjkjmljknljnljknmljkn
+      </div>
+    )
+  }
+}
+
+export default index
